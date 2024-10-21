03633235d88d07a5850bf473d2aa0f7fd3a6e8b7
Monir <mrmonir0558@gmail.com>
2024-10-21 23:46:31 +0600

    addeded some bott les card in Bottle component

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,23 +1,29 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import Bottle from "../Bottle/Bottle";
-import './Bottles.css'
+import { useEffect, useState } from 'react';
+import Bottle from '../Bottle/Bottle';
+import './Bottles.css';
 
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
+    const [cart, setCart] = useState([])
 
     useEffect(() => {
         fetch('bottles.json')
             .then(res => res.json())
-        .then(data => setBottles(data))
-    }, [])
+            .then(data => setBottles(data));
+    }, []);
+
+    const handleAddToCart = bottle => {
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+    };
 
     return (
         <div>
-            <h2>Bottles Here : {bottles.length}</h2>
-            <div className="bottle-container" >
+            <h2>Bottles Available : {bottles.length}</h2>
+            <h4>Cart: {cart.length}</h4>
+            <div className="bottle-container">
                 {bottles.map(bottle => (
-                    <Bottle key={bottle.id} bottle={bottle} />
+                    <Bottle key={bottle.id} bottle={bottle} handleAddToCart={handleAddToCart} />
                 ))}
             </div>
         </div>
